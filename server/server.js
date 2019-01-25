2dce84ec18353058f3d82274dbc792a81c4fa804
Hardik Shakya <hardik.shakya@HardikS.local>
2019-01-25 11:22:20 +0530

    Init commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+console.log(publicPath);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`Server started at port ${port}`);
+});
+
