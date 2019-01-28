6cd953450fe3e0883220df418b69acec79a87e56
Hardik Shakya <hardik.shakya@HardikS.local>
2019-01-28 14:06:27 +0530

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 // console.log(publicPath);
@@ -35,6 +35,10 @@ io.on('connection', (socket) => {
         //     text: message.text,
         //     createdAt: new Date().getTime()
         // });         
+    });
+
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
     });
 
     socket.on('disconnect', () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -15,3 +15,19 @@ describe('generateMessage', () => {
         });
     });
 });
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+      var from = 'Deb';
+      var latitude = 15;
+      var longitude = 19;
+      var url = 'https://www.google.com/maps?q=15,19';
+      var message = generateLocationMessage(from, latitude, longitude);
+  
+      expect(message.createdAt).toBeA('number');
+      expect(message).toInclude({
+          from, 
+          url
+        });
+    });
+  });
+  
