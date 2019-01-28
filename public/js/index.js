aa815ea023c0bff533918dd315f64927418491cb
Hardik Shakya <hardik.shakya@HardikS.local>
2019-01-28 12:18:01 +0530

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -14,5 +14,27 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-    console.log('New Message', message);    
+    console.log('New Message', message);  
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    
+    jQuery('#messages').append(li);
+});
+
+// socket.emit('createMessage', {
+//     from: 'Harsh',
+//     text: 'Chai pe charcha'
+// }, function (data) {
+//     console.log('Got it', data);    
+// });
+
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function () {
+        
+    });
 });
--- server/server.js
+++ server/server.js
@@ -26,9 +26,10 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
-        io.emit('newMessage', generateMessage(message.from, message.text));    
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is from the server.');    
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
