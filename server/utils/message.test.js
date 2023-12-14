86b804fcb885122c360aaf03ad3de2717c149f9c
hardikshakya <hardikshakya1997@gmail.com>
2023-12-15 00:39:16 +0530

    refactor(tests): updated message creation tests to use Jest framework

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,33 +1,35 @@
-var expect = require('expect');
-
-var {generateMessage} = require('./message');
+const { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
-    it('should generate correct message object', () => {
-        var from = 'Shyam';
-        var text = 'Kya Bolta re';
-        var message = generateMessage(from, text);
+  it('should generate the correct message object', () => {
+    const from = 'Hardik';
+    const text = 'Hey, all good?';
+    const message = generateMessage(from, text);
 
-        expect(message.createdAt).toBeA('number');
-        expect(message).toInclude({
-            from,
-            text
-        });
-    });
-});
-describe('generateLocationMessage', () => {
-    it('should generate correct location object', () => {
-      var from = 'Deb';
-      var latitude = 15;
-      var longitude = 19;
-      var url = 'https://www.google.com/maps?q=15,19';
-      var message = generateLocationMessage(from, latitude, longitude);
-  
-      expect(message.createdAt).toBeA('number');
-      expect(message).toInclude({
-          from, 
-          url
-        });
-    });
+    expect(typeof message.createdAt).toBe('number');
+    expect(message).toEqual(
+      expect.objectContaining({
+        from,
+        text,
+      })
+    );
   });
-  
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate the correct location object', () => {
+    const from = 'Hardik';
+    const latitude = 15;
+    const longitude = 19;
+    const url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+    const message = generateLocationMessage(from, latitude, longitude);
+
+    expect(typeof message.createdAt).toBe('number');
+    expect(message).toEqual(
+      expect.objectContaining({
+        from,
+        url,
+      })
+    );
+  });
+});
